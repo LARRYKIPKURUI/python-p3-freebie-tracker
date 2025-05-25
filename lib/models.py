8d0dcdb851a22b63ed0c5de64e104ac3e8c7e2b9
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-05-25 10:27:05 +0300

    Finished Creating and Mapping tables along with updating migrations

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,11 +9,18 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+# Junction table (many-to-many relationship) 
+company_dev = Table(
+    'company_dev',
+    Base.metadata,
+    Column('company_id', Integer, ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id', Integer, ForeignKey('devs.id'), primary_key=True)
+)
 class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String())
+    company_name = Column(String())
     founding_year = Column(Integer())
 
     def __repr__(self):
@@ -23,7 +30,22 @@ class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    dev_name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    
+    id = Column(Integer(), primary_key=True)
+    title = Column( String() , nullable = False)
+    item_name = Column(String(), nullable=False)
+    company_id = Column(Integer(), ForeignKey('companies.id'), nullable=False)
+    dev_id = Column(Integer(), ForeignKey('devs.id'), nullable=False)
+    
+    def __repr__(self):
+        dev_name = self.dev.name if self.dev else "Could not find Dev"
+        company_name = self.company.name if self.company else "Could not find Company"
+        return f"< {self.item_name} was won  by {dev_name}  from {company_name}>"
